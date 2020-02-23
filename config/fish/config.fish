faf0cce139d6aac41d1f28ab5df1986a893ff967
nuric <nuric@users.noreply.github.com>
2020-02-23 23:37:49 +0000

    Add basic fish shell configuration.

---
+++ config/fish/config.fish
@@ -0,0 +1,8 @@
+# Personal aliases
+alias ls "ls --color"
+alias rleft "xrandr -d :0 -o left"
+alias rnormal "xrandr -d :0 -o normal"
+
+# Custom environment variables
+set -x EDITOR "vim"
+set -x VISUAL "vim"
